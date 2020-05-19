0c9b5dc5f53bf818187cbed1999157a03db3b1fd
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-05-19 19:26:34 -0400

    add chevron

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from "react";
 import "./Accordion.css";
-// import Chevron from "./Chevron";
+import Chevron from "./Chevron";
 import ActionBtn from "./ActionBtn";
 
 const Accordion = (props) => {
   const [setActive, setActiveState] = useState("");
   const [setHeight, SetHeightState] = useState("0px");
+  const [setRotate, SetRotateState] = useState("accordion_icon");
   const content = useRef(null);
 
   const toggleAccordion = () => {
@@ -13,13 +14,16 @@ const Accordion = (props) => {
     SetHeightState(
       setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
     );
+    SetRotateState(
+      setActive === "active" ? "accordion_icon" : "accordion_icon rotate"
+    );
   };
 
   return (
     <div className="accordion_section">
       <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
         <p className="accordion_title">{props.title}</p>
-        {/* <Chevron width={10} fill={"#777"} /> */}
+        <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
       </button>
       <div
         className="accordion_content"
